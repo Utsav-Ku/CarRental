90375cd5168301700f5ca4e728ec1fb72dcc5b57
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-07-20 13:56:51 +0530

    Implement car listing with image upload using Multer and ImageKit; added API to change user role to 'owner', configured ImageKit for optimized image handling, integrated Multer with disk storage, and updated routing and models accordingly.

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,6 +1,9 @@
+import imagekit from "../configs/imageKit.js";
+import Car from "../models/Car.js";
 import User from "../models/User.js";
+import fs from 'fs';
 
-
+//Api to change user's role
 export const changeRoleToOwner = async (req, res) => {
     try {
         const { _id } = req.user;
@@ -9,6 +12,49 @@ export const changeRoleToOwner = async (req, res) => {
         res.json({success: true, message: "Now you can list cars"});
 
     } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
+
+//Api to list Car
+export const addCar = async (req, res) => {
+    try{
+        const { _id } = req.user;
+        let car = JSON.parse(req.body.carData);
+        const imageFile = req.file;
+
+        //upload image to imageKit
+        const fileBuffer = fs.readFileSync(imageFile.path);
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/cars'
+        })
+
+        // optimization through imageKit URL transformation
+        var optimizedImageUrl = imagekit.url({
+            path : response.filePath,
+            transformation : [
+                {
+                    width: '1280', //Width resizing
+                },
+                {
+                    quality: 'auto' //Auto compression
+                },
+                {
+                    format: 'webp' //Convert to modern format
+                }
+            ]
+        });
+
+        const image = optimizedImageUrl;
+
+        await Car.create({...car, owner: _id, image});
+
+        res.json({success: true, message: "Car Added"});
+
+    } catch (error){
         console.log(error.message);
         res.json({success: false, message: error.message});
     }
