367a1ac59718eb0a9d175b7d2853c5296351199e
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-07-21 12:51:08 +0530

    Refactor dashboard data API for car owners

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,4 +1,5 @@
 import imagekit from "../configs/imageKit.js";
+import Booking from "../models/Booking.js";
 import Car from "../models/Car.js";
 import User from "../models/User.js";
 import fs from 'fs';
@@ -131,6 +132,24 @@ export const getDashboardData = async (req, res) => {
         }
 
         const cars = await Car.find({owner: _id});
+        const bookings = await Booking.find({_id}).populate('car').sort({createdAt: -1});
+
+        const pendingBookings = await Booking.find({owner:_id, status: "pending"});
+        const completedBookings = await Booking.find({owner:_id, status: "confirmed"});
+
+        //Calculate monthly revenue
+        const monthlyRevenue = bookings.slice().filter(booking => booking.status === 'confirmed').reduce((acc, booking) => acc + booking.price, 0);
+
+        const dashboardData = {
+            totalCars: cars.length,
+            totalBookings: bookings.length,
+            pendingBookings: pendingBookings.length,
+            completedBookings: completedBookings.length,
+            recentBookings: bookings.slice(0, 3),
+            monthlyRevenue
+        }
+
+        res.json({ success: true, dashboardData });
         
     } catch (error) {
         console.log(error.message);
